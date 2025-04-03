c06122b7896c8e15a06e7618d1569e8888cd2868
magSeven117 <magaleanod@ut.edu.co>
2025-04-03 11:48:36 -0500

    borrar_redis
    se borra el uso de redis de manera temporal para agilizar el proceso de despliegue

--- config/database.php
+++ config/database.php
@@ -46,10 +46,10 @@ return [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '3308'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', '0000'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -141,33 +141,33 @@ return [
     |
     */
 
-    'redis' => [
+    // 'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+    //     'client' => env('REDIS_CLIENT', 'predis'),
 
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
-        ],
+    //     'options' => [
+    //         'cluster' => env('REDIS_CLUSTER', 'redis'),
+    //         'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+    //     ],
 
-        'default' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
-        ],
+    //     'default' => [
+    //         'url' => env('REDIS_URL'),
+    //         'host' => env('REDIS_HOST', '127.0.0.1'),
+    //         'username' => env('REDIS_USERNAME'),
+    //         'password' => env('REDIS_PASSWORD'),
+    //         'port' => env('REDIS_PORT', '6379'),
+    //         'database' => env('REDIS_DB', '0'),
+    //     ],
 
-        'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
-        ],
+    //     'cache' => [
+    //         'url' => env('REDIS_URL'),
+    //         'host' => env('REDIS_HOST', '127.0.0.1'),
+    //         'username' => env('REDIS_USERNAME'),
+    //         'password' => env('REDIS_PASSWORD'),
+    //         'port' => env('REDIS_PORT', '6379'),
+    //         'database' => env('REDIS_CACHE_DB', '1'),
+    //     ],
 
-    ],
+    // ],
 
 ];
